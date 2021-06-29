5a7181de5a18b49ad8e57175510a09cd793d7bd6
andrew <lhq@rentongtech.com>
2021-06-29 19:08:16 +0800

    rust

--- src/sgrep/src/main.rs
+++ src/sgrep/src/main.rs
@@ -1,16 +1,37 @@
-use std::borrow::Borrow;
 use std::env;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("在文件{}中搜索: {}", filename, query);
+    let config = Config::new(&args).map_err(|error| {
+        println!("{}", error);
+        return;
+    }).unwrap();
 
-    let contents = fs::read_to_string(filename)
+    println!("在文件{}中搜索: {}", &config.query, &config.filename);
+
+    let contents = fs::read_to_string(&config.filename)
         .expect("文件读取失败!");
 
     println!("读取到文本: {}", &contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 3 {
+            return Err(format!("命令行参数个数错误!!!, 需要{}个, 传入个数: {}", 2, args.len() - 1));
+        }
+
+        // clone new arguments
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
